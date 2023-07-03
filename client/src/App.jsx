a8a07ad79724055deb383f51f45acc6139df5821
guille <guillermoneculqueo@gmail.com>
2023-07-03 18:00:55 -0300

    redux toolkity primer slice

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Counter from './features/counter/Counter'
 
 function App () {
   return (
-    <h1>
-      web truekiando
-    </h1>
+    <>
+      <h1>web</h1>
+      <Counter />
+    </>
   )
 }
 
