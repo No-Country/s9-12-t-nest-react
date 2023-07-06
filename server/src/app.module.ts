1cf4945cc64dc86960747a13be2eeb9a9ad10467
Juan Fernandez <jmf798@gmail.com>
2023-07-05 23:23:12 -0300

    feat: categories, subcategories and product resources initialize

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,6 +5,9 @@ import { DotEnvConfig } from './config/env.config';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CategoriesModule } from './categories/categories.module';
+import { SubcategoriesModule } from './subcategories/subcategories.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
@@ -19,6 +22,9 @@ import { MongooseModule } from '@nestjs/mongoose';
       }),
       inject: [ConfigService],
     }),
+    CategoriesModule,
+    SubcategoriesModule,
+    ProductsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
