785f81996efca5683ead639a579fd73409cb7235
Juan Fernandez <jmf798@gmail.com>
2023-07-06 02:20:22 -0300

    feat: product resource finished

--- server/src/products/dto/create-product.dto.ts
+++ server/src/products/dto/create-product.dto.ts
@@ -1,1 +1,63 @@
-export class CreateProductDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
+import { IsArray, IsInt } from 'class-validator';
+import { IsString, MinLength } from 'class-validator';
+
+export class CreateProductDto {
+  @ApiProperty({
+    description: 'Item name',
+    nullable: false,
+    minLength: 1,
+  })
+  @IsString()
+  @MinLength(1)
+  name: string;
+
+  @ApiProperty({
+    description: 'Item description',
+    nullable: false,
+    minLength: 1,
+  })
+  @IsString()
+  @MinLength(1)
+  description: string;
+
+  @ApiProperty({
+    description: 'Item price',
+    nullable: false,
+    minLength: 1,
+    default: 20,
+  })
+  @Transform(({ value }) => parseInt(value))
+  @IsInt()
+  price: number;
+
+  @ApiProperty({
+    description: 'Item images',
+    nullable: false,
+  })
+  @IsArray()
+  images: string[];
+
+  @ApiProperty({
+    description: 'Item owner',
+    nullable: false,
+  })
+  @IsString()
+  owner: string;
+
+  @ApiProperty({
+    description: 'Item category',
+    nullable: false,
+  })
+  @IsString()
+  category: string;
+
+  @ApiProperty({
+    description: 'Item category',
+    nullable: false,
+  })
+  @IsArray()
+  @IsString()
+  subcategories: string[];
+}
--- server/src/products/entities/product.entity.ts
+++ server/src/products/entities/product.entity.ts
@@ -1,1 +1,35 @@
-export class Product {}
+import { Prop, SchemaFactory, Schema } from '@nestjs/mongoose';
+import { ApiProperty } from '@nestjs/swagger';
+import { Document, Types } from 'mongoose';
+
+@Schema()
+export class Product extends Document {
+  @ApiProperty()
+  @Prop()
+  name: string;
+
+  @ApiProperty()
+  @Prop()
+  description: string;
+
+  @ApiProperty()
+  @Prop({ default: 20 })
+  price: number;
+
+  @ApiProperty()
+  @Prop({ array: true })
+  images: string[];
+
+  @ApiProperty()
+  @Prop({ ref: 'User' })
+  owner: Types.ObjectId;
+
+  @ApiProperty()
+  @Prop({ ref: 'Category' })
+  category: Types.ObjectId;
+
+  @ApiProperty()
+  @Prop({ array: true, ref: 'Subcategory' })
+  subcategories: Types.ObjectId[];
+}
+export const ProductSchema = SchemaFactory.createForClass(Product);
