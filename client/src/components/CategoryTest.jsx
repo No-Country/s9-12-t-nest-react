577dc74d14dd4f625846164ef5d8b9e68269f48e
guille <guillermoneculqueo@gmail.com>
2023-07-06 03:37:22 -0300

    añadiendo funciones en el slice, parafiltrar por ..

--- client/src/components/CategoryTest.jsx
+++ client/src/components/CategoryTest.jsx
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from 'react'
-
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchCategories, fetchProductsByCategory } from '../features/products/fetchProducts'
 import { v4 as uuidv4 } from 'uuid'
-
 import Box from '@mui/material/Box'
 import InputLabel from '@mui/material/InputLabel'
 import MenuItem from '@mui/material/MenuItem'
 import FormControl from '@mui/material/FormControl'
 import Select from '@mui/material/Select'
 import Loading from './Loading'
+import ProductsCard from './ListProduct/ProductsCard'
+import { Grid } from '@mui/material'
 
 const CategoryTest = () => {
   const categories = useSelector((state) => state?.products?.category)
@@ -45,8 +45,9 @@ const CategoryTest = () => {
       })
   }, [categori])
 
+  console.log(filtrados)
   return (
-    <>
+    <div style={{ justifyContent: 'center', alignItems: 'center', display: 'flex', flexDirection: 'column', gap: '20px' }}>
       <h2>Categorias</h2>
       <div sx={{ flexDirection: 'row', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
         <Box sx={{ minWidth: 120, maxWidth: 160 }}>
@@ -77,15 +78,22 @@ const CategoryTest = () => {
 
       <div>
         <h2>productos filtrados por categoria</h2>
-        {filtrados
-          ? (filtrados.map((obj) =>
-            <p key={uuidv4()}>{obj.title}</p>
-            ))
+        <Grid container spacing={2}>
+          {filtrados.length > 0
+            ? (filtrados.map((obj) =>
+              <ProductsCard key={uuidv4()} producto={obj} />
+              ))
+            : <p>No selecciono ningunacategoria</p>}
 
-          : <p>no hay product</p>}
+          {filtrados
+            ? (filtrados.map((obj) =>
+              <ProductsCard key={uuidv4()} producto={obj} />
+              ))
+            : <p>No selecciono ninguna categoria</p>}
+        </Grid>
 
       </div>
-    </>
+    </div>
   )
 }
 
--- client/src/dbJson/fetchProducts.js
+++
@@ -1,9 +0,0 @@
-import dbProduct from './db_stockJSON'
-
-const getData = async () => {
-  const response = await fetch(dbProduct)
-  const data = await response.json()
-  return data
-}
-
-export default getData
