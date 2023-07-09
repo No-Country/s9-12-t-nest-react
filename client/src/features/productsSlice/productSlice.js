b27bafdbf6130f507ed305371b034925f06331b2
guille <guillermoneculqueo@gmail.com>
2023-07-09 20:12:36 -0300

    acciones asincronas para CRUD para gestion de producto

--- client/src/features/productsSlice/productSlice.js
+++ client/src/features/productsSlice/productSlice.js
@@ -131,16 +131,89 @@ export const deleteProductById = createAsyncThunk('products/delete',
     }
   })
 
+export const getProductsByCategoryId = createAsyncThunk('products/getProductsByCategoryId', async (categoryId, thunkAPI) => {
+  try {
+    const response = await fetch(`${API_URL}/category/${categoryId}`, {
+      method: 'GET',
+      headers: {
+        'Content-type': 'application/json'
+      }
+    })
+    if (!response.ok) {
+      if (response.status === 404) {
+        return thunkAPI.rejectWithValue({ status: response.status, message: 'Productos con id de categoría no encontrados' })
+      } else {
+        const error = await response.text()
+        return thunkAPI.rejectWithValue(error)
+        // throw new Error('Algo salio mal')
+      }
+    }
+    const data = await response.json()
+    return data
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error)
+  }
+})
+
+export const getProductsBySubcategoryId = createAsyncThunk('products/getProductsBySubcategoryId', async (subCategoryId, thunkAPI) => {
+  try {
+    const response = await fetch(`${API_URL}/subcategories/${subCategoryId}`, {
+      method: 'GET',
+      headers: {
+        'Content-type': 'application/json'
+      }
+    })
+    if (!response.ok) {
+      if (response.status === 404) {
+        return thunkAPI.rejectWithValue({ status: response.status, message: 'Productos con ID de subcategoría no encontrados' })
+      } else {
+        const error = await response.text()
+        return thunkAPI.rejectWithValue(error)
+        // throw new Error('Algo salio mal')
+      }
+    }
+    const data = await response.json()
+    return data
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error)
+  }
+})
+
 const productSlice = createSlice({
   name: 'products',
   initialState: {
     products: [],
+    productById: [],
+    productsByCategory: [],
+    productsBySubcategory: [],
+    productsByKeyword: [],
     status: '',
     loading: false,
     error: null
   },
   reducers: {
-
+    addToproductsByKeyword: (state, action) => {
+      state.productsByKeyword = []
+      state.productsByKeyword = action.payload
+    },
+    clearProductById: (state) => {
+      state.productById = []
+    },
+    clearProductsByCategory: (state) => {
+      state.productsByCategory = []
+    },
+    clearProductsBySubcategory: (state) => {
+      state.productsBySubcategory = []
+    },
+    clearProductByKeyword: (state) => {
+      state.productsByKeyword = []
+    },
+    clearAllFilters: (state) => {
+      state.productById = []
+      state.productsByCategory = []
+      state.productsBySubcategory = []
+      state.productsByKeyword = []
+    }
   },
   extraReducers: (builder) => {
     // manejo acciones asincronas
@@ -194,7 +267,7 @@ const productSlice = createSlice({
       .addCase(getProductById.fulfilled, (state, action) => {
         state.status = 'succeeded'
         state.loading = false
-        state.products = action.payload
+        state.productById = action.payload
       })
       .addCase(getProductById.rejected, (state, action) => {
         state.status = 'failed'
@@ -246,12 +319,73 @@ const productSlice = createSlice({
           state.error = 'Ocurrión un error al eliminar el producto.'
         }
       })
+      .addCase(getProductsByCategoryId.pending, (state) => {
+        state.status = 'loading'
+        state.loading = true
+      })
+      .addCase(getProductsByCategoryId.fulfilled, (state, action) => {
+        state.status = 'succeeded'
+        state.loading = false
+        state.productsByCategory = action.payload
+      })
+      .addCase(getProductsByCategoryId.rejected, (state, action) => {
+        state.status = 'failed'
+        state.loading = false
+        state.error = action.payload
+        if (action.payload && action.payload.status === 404) {
+          state.error = action.payload.message
+        } else {
+          state.error = 'Ocurrión un error al obtener los productos.'
+        }
+      })
+      .addCase(getProductsBySubcategoryId.pending, (state) => {
+        state.status = 'loading'
+        state.loading = true
+      })
+      .addCase(getProductsBySubcategoryId.fulfilled, (state, action) => {
+        state.status = 'succeeded'
+        state.loading = false
+        state.productsBySubcategory = action.payload
+      })
+      .addCase(getProductsBySubcategoryId.rejected, (state, action) => {
+        state.status = 'failed'
+        state.loading = false
+        state.error = action.payload
+        if (action.payload && action.payload.status === 404) {
+          state.error = action.payload.message
+        } else {
+          state.error = 'Ocurrión un error al obtener los productos.'
+        }
+      })
   }
 })
 
+export const { addToproductsByKeyword, clearProductById, clearProductsByCategory, clearProductsBySubcategory, clearProductByKeyword, clearAllFilters } = productSlice.actions
 export const selectProducts = (state) => state.product.products
-// export const selectStatus = (state) => state.products.status
-// export const selectLoading = (state) => state.products.loading
-// export const selectError = (state) => state.products.error
-// export const selectCategory = (state) => state.products.category
 export default productSlice.reducer
+
+/*
+Nota:
+al haber tantas formas de filtrar creo que lo mas conveniente es hacerlo algo asi:
+
+* en caso de querer filtar todo en el home
+if (productByCategory.length > 0 && productsBySubcategory.length === 0 && productById.length === 0 &&  productsByKeyword.length === 0 ) {
+    Mostrar los productos filtrados por categoría
+  } else if (productById.length > 0 &&  productByCategory.length === 0 && productsBySubcategory.length === 0 &&  productsByKeyword.length === 0 ){
+    Mostrar los productos filtrados por ID
+  }
+  else if ( productsByKeyword.length > 0 && productById.length === 0 &&  productByCategory.length === 0 && productsBySubcategory.length === 0 ){
+    Mostrar los productos filtrados por palabra clave
+  }
+  else {
+    mostramos todos los productos
+  }
+
+  * la otra es añadir rutas para cada filtrado, quizas sea lo mejor opcion
+  /  => filtra todos
+  /filtroId  => filtrara por id
+  /filtroCategoryId => filtra por id de la categoria
+  /filtroSubcategoryId => filtra por id de la subcategoria
+  /filtroKeyword => filtra por palabra clave
+
+*/
