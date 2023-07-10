27025fa758080d4dc51270a49fd014b8efbea2c6
guille <guillermoneculqueo@gmail.com>
2023-07-09 23:01:51 -0300

    solucion erroor nombre

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,16 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from '../features/counter/counterSlice'
 import productsReducer from '../features/products/fetchProducts'
 import authReducer from '../features/authSlice/authSlice'
 import categoriesReducer from '../features/categoriesSlice/categorySlice'
-// import productReducer from '../features/productsSlice/productSlice'
+import productReducer from '../features/productsSlice/productSlice'
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     products: productsReducer,
     authUser: authReducer,
+    productsDb: productReducer,
     categories: categoriesReducer
-    // products: productReducer
   }
 })
