f2ec2ccd3e456ea10e1ec02c89725a2c8e3fce48
guille <guillermoneculqueo@gmail.com>
2023-07-09 22:11:08 -0300

    slice para CRUD de usuario

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -2,11 +2,15 @@ import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from '../features/counter/counterSlice'
 import productsReducer from '../features/products/fetchProducts'
 import authReducer from '../features/authSlice/authSlice'
+import categoriesReducer from '../features/categoriesSlice/categorySlice'
+// import productReducer from '../features/productsSlice/productSlice'
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     products: productsReducer,
-    authUser: authReducer
+    authUser: authReducer,
+    categories: categoriesReducer
+    // products: productReducer
   }
 })
