f1586e31ec898e552235dd397fd37164229a5f14
guille <guillermoneculqueo@gmail.com>
2023-07-12 18:45:05 -0300

    stars components

--- client/src/pages/Perfil/PerfilUser.jsx
+++ client/src/pages/Perfil/PerfilUser.jsx
@@ -46,18 +46,18 @@ const PerfilUser = () => {
 
   return (
     <div className='container principalPerfil p-3 d-flex justify-content-center align-items-center'>
-      <div className='card d-flex flex-column justify-content-center align-items-center align-content-center flex-nowrap gap-2' style={{ width: '100%', height: 'auto' }}>
+      <div className='card d-flex flex-column justify-content-center align-items-center align-content-center flex-nowrap gap-3' style={{ width: '100%', height: 'auto' }}>
         {/* head perfil */}
-        <section className='imgName d-flex flex-row justify-content-center align-items-center align-content-center flex-nowrap gap-3 position-relative' style={{ width: '100%', height: '100px' }}>
+        <section className='imgName d-flex flex-row justify-content-center align-items-center align-content-center flex-nowrap gap-3 position-relative' style={{ width: '100%', height: '70px' }}>
           {/* img Perfil left */}
           <div className='profile overflow-hidden position-static rounded-circle overflow-hidden' style={{ width: '64.837px', height: '64.837px' }}>
             <img src={objeto.img} style={{ width: '100%', height: '100%' }} />
           </div>
           {/* info Perfil right  */}
           <section className='titulos position-static d-flex flex-column justify-content-center align-items-start align-content-center flex-nowrap gap-2'>
-            <div className='d-flex flex-row justify-content-start align-items-center align-content-center flex-nowrap gap-2 ' style={{ fontFamily: 'var(--titulo)' }}>
+            <div className='d-flex flex-row justify-content-start align-items-center align-content-center flex-nowrap gap-3 ' style={{ fontFamily: 'var(--titulo)' }}>
               <h2 className='m-0 p-0' style={{ color: '#333', fontFamily: 'var(--titulo)', fontSize: '20px', fontWeight: '600' }}>{objeto.firstName}</h2>
-              <span className='d-flex flex-row justify-content-center align-items-center align-content-center flex-nowrap' style={{ width: '30.512px', height: '30.512px', borderRadius: '50%', fontSize: '1.5rem', backgroundColor: 'var(--background-nav)', color: 'white' }}>
+              <span className='d-flex flex-row justify-content-center align-items-center align-content-center flex-nowrap' style={{ width: '30.512px', height: '30.512px', borderRadius: '50%', fontSize: '1.3rem', backgroundColor: 'var(--background-nav)', color: 'white' }}>
                 <ion-icon name='checkmark-sharp' />
               </span>
             </div>
@@ -86,9 +86,10 @@ const PerfilUser = () => {
 
         {/* Estrellas valoracion */}
         <Estrellas number={calculaRep} />
+
         {/* Calificacion Usuario  */}
-        <div className=' text-center'>
-          <div className='d-flex justify-content-between align-items-center mt-4 px-4'>
+        <section className='valoresRep' style={{ width: '100%', height: 'auto' }}>
+          <div className='d-flex justify-content-between align-items-center ' style={{ background: 'var(--yellowContenedor)' }}>
             <div className='stats'>
               <span>{calculaRep}</span>
               <h3 className='mb-0'>Reputacion</h3>
@@ -102,7 +103,7 @@ const PerfilUser = () => {
               <h3 className='mb-0'>Exitosos</h3>
             </div>
           </div>
-        </div>
+        </section>
       </div>
     </div>
   )
