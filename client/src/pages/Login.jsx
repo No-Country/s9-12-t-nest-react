5e87ebe4b717d961691cf9a0889c9155ad94450a
Sofía Pereyra <sofi.pereyra23@gmail.com>
2023-07-14 20:40:07 -0300

    Update login and register functionality

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import '../pages/styles/UserRegisterLogin.css'
+import { AuthService } from '../../../server/src/auth/auth.service'
 
 const initialValues = {
     email: "",
@@ -9,20 +10,79 @@ const initialValues = {
 
 function Login() {
     const [state, setState] = useState(initialValues);
-    const [errors, setErrors] = useState({});
+    const [errors, setErrors] = useState([]);
+    const [showPassword, setShowPassword] = useState(false);
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
 
     const handleChange = (e) => {
         setState({ ...state, [e.target.name]: e.target.value });
     };
 
+    const validate = () => {
+        let errorsList = {};
+        const emailRegExp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+        if (!state.email)
+            errorsList = { ...errorsList, email: "Campo obligatorio" };
+        else if (!emailRegExp.test(state.email))
+            errorsList = {
+                ...errorsList,
+                email: "Debe ingresar una direccion de correo válida",
+            };
+
+        return errorsList;
+    }
+
     const handleSubmit = async (e) => {
         e.preventDefault();
+        const res = validate();
+        if (Object.keys(res).length < 1) {
+            try {
+                const user = {
+                    email: state.email,
+                    password: state.password
+                }
+                const response = await fetch("http://localhost:3000/api/v1/auth/login", {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(user)
+                });
+                if (!response.ok) throw new Error("Datos inválidos, intenta nuevamente")
+
+            } catch (error) {
+                setErrors(error)
+                console.log(error);
+            }
+        }
+        setErrors(res)
     };
 
+    const googleLogin = async (e) => {
+        try {
+            const user = {
+                email: state.email,
+                password: state.password
+            }
+            const response = await fetch("http://localhost:3000/api/v1/auth/google/callback", {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
-        <main style={{ textAlign: "center", margin:"20px"}}>
+        <main style={{ textAlign: "center", margin: "20px" }}>
             <img className="logo-img" src="/images/logo-login.svg" alt="Trueka logo image" />
-            <p className='slogan'>Encontrá lo que buscás, <br/>
+            <p className='slogan'>Encontrá lo que buscás, <br />
                 cambialo por lo que ya no necesitás</p>
             <h5>Iniciar Sesión</h5>
             <form onSubmit={handleSubmit} className='form'>
@@ -30,18 +90,30 @@ function Login() {
                     <input
                         id="email"
                         placeholder="Correo Electrónico"
+                        name='email'
+                        value={state.email}
                         onChange={handleChange}
                     />
-                    <div class="pass-positioning">
-                        <label for="password"></label>
+                    {errors.email && <p className='error-msg'>{errors.email}</p>}
+
+                    <div className="pass-positioning">
+                        <label htmlFor="password"></label>
                         <input
                             id="password"
                             placeholder="Contraseña"
-                            type="password"
+                            name='password'
+                            value={state.password}
+                            type={showPassword ? 'text' : 'password'}
                             onChange={handleChange}
                         />
-                        <img id="eye-img" src="/images/visibility.svg" alt="Eye icon to show or hidden password"/>
+                        <img
+                            id="eye-img"
+                            src="/images/visibility.svg"
+                            alt="Eye icon to show or hidden password"
+                            onClick={togglePasswordVisibility}
+                        />
                     </div>
+                    {errors.length >= 1 && <p className='error-msg'>{errors}</p>}
                     <div className='forgot-pass'>
                         <Link to="/login" >Olvidé mi contraseña</Link>
                     </div>
@@ -50,7 +122,7 @@ function Login() {
                     >Iniciar Sesión</button>
                     <div className='new-account'>
                         <span className="mt-2">
-                        ¿No tenés una cuenta? </span>
+                            ¿No tenés una cuenta? </span>
                         <Link
                             to="/register"
                         >
@@ -60,7 +132,7 @@ function Login() {
                     <hr id="hr" />
                     <div className='login-alternatives'>
                         <p>Ingresar con</p>
-                        <img src="/images/google.svg" alt="Google icon" />
+                        <img src="/images/google.svg" alt="Google icon" onClick={googleLogin} />
                         <img src="/images/facebook.svg" alt="Facebook icon" />
                     </div>
                 </div>
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,9 +1,13 @@
 import React, { useState } from 'react'
 import '../pages/styles/UserRegisterLogin.css'
+import { createUser } from '../features/authSlice/authSlice';
 
 const initialValues = {
     name: "",
+    lastName:"",
     email: "",
+    contact: "",
+    address: "",
     password: "",
     password2: ""
 }
@@ -12,63 +16,137 @@ function Register() {
     const [state, setState] = useState(initialValues);
     const [errors, setErrors] = useState({});
 
+    const validate = () => {
+        let errorsList = {};
+        const emailRegExp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+        if (!state.email)
+        errorsList = { ...errorsList, email: "Campo obligatorio" };
+        else if (!emailRegExp.test(state.email))
+        errorsList = {
+            ...errorsList,
+            email: "Debe ingresar una direccion de correo válida",
+        };
+        if (!state.name) errorsList = { ...errorsList, name: "Campo obligatorio" };
+        if (!state.lastName)
+        errorsList = { ...errorsList, lastName: "Campo obligatorio" };
+        if (!state.contact)
+        errorsList = { ...errorsList, contact: "Campo obligatorio" };
+        if (!state.address)
+        errorsList = { ...errorsList, address: "Campo obligatorio" };
+
+        if (state.password !== state.password2)
+        errorsList = {
+            ...errorsList,
+            password: ["Los contraseñas deben coincidir"],
+            password2: ["Los contraseñas deben coincidir"],
+        };
+
+        return errorsList;
+    }
+
     const handleChange = (e) => {
         setState({ ...state, [e.target.name]: e.target.value });
     };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        const response = validate();
+        if (Object.keys(response).length < 1) {
+            try {
+                const body = {
+                    name: state.name,
+                    lastName: state.lastName,
+                    email: state.email,
+                    password: state.password,
+                    address: state.address,
+                    contact: state.contact
+                }
+                createUser(body);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        setErrors(response)
+        
     };
 
     return (
         <main>
-            <h3 style={{ textAlign: "center", margin:"20px"}}>Crear una cuenta</h3>
+            <h3 style={{ textAlign: "center", marginTop:"20px"}}>Crear una cuenta</h3>
             <form
                 className='form'
                 onSubmit={handleSubmit}
             >
                 <div className='form-content'>
+                    <label htmlFor='name'>Nombres</label>
+                    <input                     
+                        id="name"
+                        placeholder="Nombres"
+                        name="name"
+                        onChange={handleChange}
+                        defaultValue={state.name}
+                    />
+                    {errors.name && <p className='error-msg'>{errors.name}</p>}
+                    <label htmlFor='lastName'>Apellido</label>
+                    <input                   
+                        id="lastName"
+                        placeholder="Apellido"
+                        name="lastName"
+                        onChange={handleChange}
+                        defaultValue={state.lastName}
+                    />
+                    {errors.lastName && <p  className='error-msg'>{errors.lastName}</p>}
+                    <label htmlFor='contact'>Número de contacto</label>
+                    <input                     
+                        id="contact"
+                        placeholder="Número de contacto"
+                        name="contact"
+                        onChange={handleChange}
+                        defaultValue={state.contact}
+                    />
+                    {errors.contact && <p className='error-msg'>{errors.contact}</p>}
+                    <label htmlFor='address'>Dirección</label>
+                    <input                     
+                        id="address"
+                        placeholder="Dirección"
+                        name="address"
+                        onChange={handleChange}
+                        defaultValue={state.address}
+                    />
+                    {errors.address && <p className='error-msg'>{errors.address}</p>}
                     <label 
                         htmlFor='email'>Correo Electrónico
                     </label>
                     <input
                         id="email"
                         placeholder="Correo Electrónico"
+                        name="email"
+                        value={state.email}
                         onChange={handleChange}
                     />
+                    {errors.email && <p className='error-msg'>{errors.email}</p>}
                     <label htmlFor='password'>Contraseña</label>
                     <input
                         placeholder="Contraseña"
                         id="password"
                         type="password"
+                        name='password'
                         onChange={handleChange}
+                        defaultValue={state.password}
                     />
+                    {errors.password && <p className='error-msg'>{errors.password}</p>}
                     <label htmlFor='password2'>Repetir contraseña</label>
                     <input
                         id="password2"
                         placeholder="Repetir Contraseña"
                         type="password"
+                        name='password2'
                         onChange={handleChange}
+                        defaultValue={state.password2}
                     />
-                    <label htmlFor='name'>Nombres</label>
-                    <input                     
-                        id="name"
-                        placeholder="Nombres"
-                        onChange={handleChange}
-                    />
-                    <label htmlFor='lastName'>Apellido</label>
-                    <input                   
-                        id="lastName"
-                        placeholder="Apellido"
-                        onChange={handleChange}
-                    />
-                    <label htmlFor='userName'>Nombre de Usuario</label>
-                    <input              
-                        id="userName"
-                        placeholder="Hasta 20 caracteres"
-                        onChange={handleChange}
-                    />
-                    <div style={{textAlign:"center"}}>
+                    {errors.password2 && <p className='error-msg'>{errors.password2}</p>}
+                    <div style={{textAlign:"center", marginTop:"20px"}}>
                         <button
                             className='button'
                             type="submit"
