2e16f8dd0684417e2b3e55d53d87a1085ec3aad6
Dylan Navarro <dylanrubennavarro@hotmail.com>
2023-07-17 15:11:53 -0300

    chore: remove Pagination module

--- client/src/components/carousel/Carousel.jsx
+++ client/src/components/carousel/Carousel.jsx
@@ -1,27 +1,29 @@
 import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/css'
 import CardProduct from '../CardProduct'
-import { Pagination } from 'swiper/modules'
 import '../carousel/carousel.css'
 
 function Carousel ({ data }) {
   return (
     <>
-      <Swiper
-        slidesPerView='auto'
-        spaceBetween={20}
-        pagination={{
-          clickable: true
-        }}
-        modules={[Pagination]}
-        className='mySwiper'
-      >
-        {data.map((product, i) => (
-          <SwiperSlide className='swiper-slide' key={i}>
-            <CardProduct props={[product]} className='swiper-card-product' />
-          </SwiperSlide>
-        ))}
-      </Swiper>
+      <div className='card-carousel-container'>
+        <Swiper
+          slidesPerView='auto'
+          spaceBetween={20}
+          pagination={{
+            clickable: true
+          }}
+          // modules={[Pagination]}
+          className='mySwiper'
+        >
+          {data.map((product, i) => (
+            <SwiperSlide className='swiper-slide' key={i}>
+              <CardProduct props={[product]} className='swiper-card-product' />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+
     </>
   )
 }
