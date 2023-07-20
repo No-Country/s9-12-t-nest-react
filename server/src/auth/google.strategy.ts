99600d0cf4b0be7ceab3fa63b1b8db3d434dcc53
Matias Ferrari <matiasferrari96@gmail.com>
2023-07-20 16:05:54 -0300

    loginGoogle

--- server/src/auth/google.strategy.ts
+++ server/src/auth/google.strategy.ts
@@ -1,20 +1,25 @@
-import { Injectable } from "@nestjs/common";
-import { PassportStrategy } from "@nestjs/passport";
-import {Strategy, VerifyCallback} from 'passport-google-oauth20'
-import { GOOGLE_CLIENT_ID, GOOGLE_SECRET } from "src/constants";
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, VerifyCallback } from 'passport-google-oauth20';
+import { GOOGLE_CLIENT_ID, GOOGLE_SECRET } from 'src/constants';
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy) {
-    constructor() {
-        super({
-            clientID: GOOGLE_CLIENT_ID,
-            clientSecret: GOOGLE_SECRET,
-            callbackURL: 'http://localhost:3000/api/v1/auth/google/callback',
-            scope: ['email', 'profile']
-        })
-    }
+  constructor() {
+    super({
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_SECRET,
+      callbackURL: 'http://localhost:5173/',
+      scope: ['email', 'profile'],
+    });
+  }
 
-    async validate (accessToken: string, refreshToken: string, profile: any, done: VerifyCallback): Promise<any> {
-        done(null, profile);
-    }
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: any,
+    done: VerifyCallback,
+  ): Promise<any> {
+    done(null, profile);
+  }
 }
