532e9a0fbd13993d46ef85b557d8a8670da05e61
Dylan Navarro <dylanrubennavarro@hotmail.com>
2023-07-21 02:04:13 -0300

    feat: add new functions to component

--- client/src/components/MapWithSearch/MapWithSearch.jsx
+++ client/src/components/MapWithSearch/MapWithSearch.jsx
@@ -1,49 +1,60 @@
-import React, { useState } from 'react'
 import axios from 'axios'
+import { useDispatch } from 'react-redux'
+import { setLat } from '../../features/location/location'
 
-const MapWithSearch = () => {
-  const [location, setLocation] = useState(null)
-  const [searchTerm, setSearchTerm] = useState('')
+const MapWithSearch = ({ onInputChange, valueSearch }) => {
+  const dispatch = useDispatch()
 
   const handleSearch = async () => {
-    try {
-      const response = await axios.get(
-        'https://maps.googleapis.com/maps/api/geocode/json',
-        {
-          params: {
-            address: searchTerm,
-            key: 'AIzaSyB4ehacPzGTBQ4iCrRK0APPkq8u7oMD4L8'
+    if (valueSearch !== '') {
+      try {
+        const response = await axios.get(
+          'https://maps.googleapis.com/maps/api/geocode/json',
+          {
+            params: {
+              address: valueSearch,
+              key: 'AIzaSyB4ehacPzGTBQ4iCrRK0APPkq8u7oMD4L8'
+            }
           }
-        }
-      )
+        )
 
-      if (response.data.results.length > 0) {
-        const { lat, lng } = response.data.results[0].geometry.location
-        setLocation({ lat, lng })
-      } else {
-        setLocation(null)
+        if (response.data.results.length > 0) {
+          const { lat, lng } = response.data.results[0].geometry.location
+          localStorage.setItem('latitude', lat)
+          localStorage.setItem('longitude', lng)
+
+          setTimeout(() => {
+            dispatch(setLat(lat))
+          }, 300)
+        } else {
+          console.log('no se obtuvieron resultados')
+        }
+      } catch (error) {
+        console.error('Error al obtener la ubicación:', error)
       }
-    } catch (error) {
-      console.error('Error al obtener la ubicación:', error)
     }
   }
 
   return (
-    <div>
-      <input
-        type='text'
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-        placeholder='Ingresa una ubicación...'
-      />
-      <button onClick={handleSearch}>Buscar</button>
-      {location && (
-        <div>
-          <p>Latitud: {location.lat}</p>
-          <p>Longitud: {location.lng}</p>
-        </div>
-      )}
-    </div>
+  // <div>
+    <input
+      type='text'
+      // value={searchTerm}
+      onChange={onInputChange}
+      placeholder='Ej:Pais, Ciudad, Localidad'
+      onBlur={handleSearch}
+      name='location'
+      className='form-control'
+      id='input-bot'
+    />
+  //   <button onClick={handleSearch}>Buscar</button>
+  //   {location && (
+  //     <div>
+  //       <p>Latitud: {location.lat}</p>
+  //       <p>Longitud: {location.lng}</p>
+  //     </div>
+  //   )}
+  // </div>
   )
 }
 
