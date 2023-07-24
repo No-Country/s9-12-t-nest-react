d465891e656ed54e099c8867ac54951e32c94aed
Dylan Navarro <dylanrubennavarro@hotmail.com>
2023-07-24 19:27:34 -0300

    refactor: add conditional render on cardProduct - path

--- client/src/components/CardProduct.jsx
+++ client/src/components/CardProduct.jsx
@@ -1,41 +1,75 @@
+/* eslint-disable react/jsx-closing-tag-location */
+/* eslint-disable react/jsx-indent */
 import { Link } from 'react-router-dom'
 import '../components/CardProduct.css'
 import LocationSVG from './LocationSVG'
 import LocationSVGMobile from './LocationSVGMobile'
+import { useEffect } from 'react'
 
-export default function card (products) {
+export default function CardProduct (products) {
   const handleInfo = (value) => {
     localStorage.setItem('geo', JSON.stringify(value.geolocation))
   }
 
+  const currentPath = window.location.pathname.startsWith('/ofertar/')
+
   return (
     <>
-      {products.props.map((product, i) => (
-        <Link to={`/detalle/${product._id}/${product.owner}`} className='card-link' key={i} onClick={() => handleInfo(product)}>
+      {currentPath
+        ? (<>
+          {products.props.map((product, i) => (
 
-          <div className='container-card'>
-            <div className='img-container '>
-              <img src={product.images} className='card-img-top' alt='product-image' />
-            </div>
-            <div className='card-grid'>
-              <p className='text-card'>{product.name}</p>
-              <div className='location-container'>
-                <div id='desktop-svg'>
-                  <LocationSVG />
+              <div className='container-card' key={i}>
+                <div className='img-container '>
+                  <img src={product.images} className='card-img-top' alt='product-image' />
                 </div>
-                <div id='mobile-svg'>
-                  <LocationSVGMobile />
-                </div>
-                <div className='text-container'>
-                  <p className='ubi-text'>{product.location}</p>
-                </div>
+                <div className='card-grid'>
+                  <p className='text-card'>{product.name}</p>
+                  <div className='location-container'>
+                    <div id='desktop-svg'>
+                      <LocationSVG />
+                    </div>
+                    <div id='mobile-svg'>
+                      <LocationSVGMobile />
+                    </div>
+                    <div className='text-container'>
+                      <p className='ubi-text'>{product.location}</p>
+                    </div>
 
+                  </div>
+
+                </div>
               </div>
+          ))}
+        </>)
+        : (<>
+          {products.props.map((product, i) => (
+            <Link to={`/detalle/${product._id}/${product.owner}`} className='card-link' key={i} onClick={() => handleInfo(product)}>
 
-            </div>
-          </div>
-        </Link>
-      ))}
+              <div className='container-card'>
+                <div className='img-container '>
+                  <img src={product.images} className='card-img-top' alt='product-image' />
+                </div>
+                <div className='card-grid'>
+                  <p className='text-card'>{product.name}</p>
+                  <div className='location-container'>
+                    <div id='desktop-svg'>
+                      <LocationSVG />
+                    </div>
+                    <div id='mobile-svg'>
+                      <LocationSVGMobile />
+                    </div>
+                    <div className='text-container'>
+                      <p className='ubi-text'>{product.location}</p>
+                    </div>
+
+                  </div>
+
+                </div>
+              </div>
+            </Link>
+          ))}
+           </>)}
 
     </>
   )
