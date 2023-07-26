231d0203fc191da6f7492adf502815f88d5ffd99
guille <guillermoneculqueo@gmail.com>
2023-07-25 21:13:55 -0300

    cabios a perfil prueba

--- client/src/pages/Perfil/UserProductCard/CardPrueba.jsx
+++ client/src/pages/Perfil/UserProductCard/CardPrueba.jsx
@@ -2,9 +2,15 @@ import React, { useState } from 'react'
 import './card.css'
 // import { Link } from 'react-router-dom'
 import BotonCardHeart from '../BotonCardHeart/BotonCardHeart'
+import { useNavigate } from 'react-router-dom'
 
 const UserProductCard = ({ element, funcDeletes }) => {
   const [ishover, setIshover] = useState(false)
+  const navigate = useNavigate()
+
+  const handleNavigateDetail = () => {
+    navigate(`/detalle/${element._id}/${element.owner}`)
+  }
 
   const handleMoueEnter = () => {
     // console.log('DENTRO de mouse enter')
@@ -20,7 +26,7 @@ const UserProductCard = ({ element, funcDeletes }) => {
     <>
       <div className='cardCustomer' onMouseEnter={handleMoueEnter} onMouseLeave={handleMoueLeave}>
         <div className={`customContent activada ${ishover ? '' : 'hovered'}`}>
-          <div className='back'>
+          <div className='back' onClick={handleNavigateDetail} style={{ cursor: 'pointer' }}>
             <div className='back-content'>
               <section className='imgCardPrefil'>
                 <img className='imagenCard' src={element.images[0]} alt={element.description} />
