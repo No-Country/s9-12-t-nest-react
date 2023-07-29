51c68c10fdc434f19f5a55663d03ab8827aa2604
guille <guillermoneculqueo@gmail.com>
2023-07-29 20:53:24 -0300

    ya anda el watsapp, traigo el contacto desde user

--- client/src/pages/Perfil/CardOwnerOffer/CardMiniPerfilOfertas/CardMiniPerfilOfertas.jsx
+++ client/src/pages/Perfil/CardOwnerOffer/CardMiniPerfilOfertas/CardMiniPerfilOfertas.jsx
@@ -17,12 +17,12 @@ const CardMiniPerfilOfertas = ({ dueñoOfertaId, ubicacionOferta }) => {
       <section className='imgName d-flex flex-row justify-content-center align-items-center align-content-center flex-nowrap gap-3 position-relative' style={{ width: '100%', height: '70px' }}>
         {/* img Perfil left */}
         <div className='profile overflow-hidden position-static rounded-circle overflow-hidden' style={{ width: '64.837px', height: '64.837px' }}>
-          <img src={usuario?.picture} style={{ width: '100%', height: '100%' }} />
+          <img src={usuario?.picture || 'https://cdn.icon-icons.com/icons2/1508/PNG/512/systemusers_104569.png'} style={{ width: '100%', height: '100%' }} />
         </div>
         {/* info Perfil right  */}
         <section className='titulos position-static d-flex flex-column justify-content-center align-items-start align-content-center flex-nowrap gap-2'>
           <div className='d-flex flex-row justify-content-start align-items-center align-content-center flex-nowrap gap-3 ' style={{ fontFamily: 'var(--titulo)' }}>
-            <h2 className='m-0 p-0' style={{ color: '#333', fontFamily: 'var(--titulo)', fontSize: '20px', fontWeight: '600' }}>{usuario?.firstName}</h2>
+            <h2 className='m-0 p-0' style={{ color: '#333', fontFamily: 'var(--titulo)', fontSize: '20px', fontWeight: '600' }}>{usuario?.firstName || 'name'}</h2>
             <span className='d-flex flex-row justify-content-center align-items-center align-content-center flex-nowrap' style={{ width: '30.512px', height: '30.512px', borderRadius: '50%', fontSize: '1.3rem', backgroundColor: 'var(--background-nav)', color: 'white' }}>
               <ion-icon name='checkmark-sharp' />
             </span>
@@ -41,7 +41,7 @@ const CardMiniPerfilOfertas = ({ dueñoOfertaId, ubicacionOferta }) => {
                 <>
                   <ion-icon name='location-sharp' style={{ color: 'var(--background-nav)' }} />
                   <h3 className='text-muted d-block p-0 m-0' style={{ fontSize: '14px' }}>
-                    {ubicacionOferta}
+                    {ubicacionOferta || 'Ubicación'}
                   </h3>
                 </>
                 )}
